145c24dfb59dc9f3febb65e16d6b00bbc9870f7c
sf17490 <sflynn@duck.com>
2025-05-17 15:04:47 +0100

    Remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,5 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        // brand: "#1E40AF", // Custom brand color (a deep blue)
-        // snowWhite: "#ffffff",
-      },
-    },
-  },
   plugins: [],
 };
