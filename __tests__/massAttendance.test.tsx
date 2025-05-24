d88a48f6f2b056c8161c9c986c19592bcf2bc7e9
sf17490 <sflynn@duck.com>
2025-05-24 18:01:38 +0100

    Fix massAttendance test imports

--- __tests__/massAttendance.test.tsx
+++ __tests__/massAttendance.test.tsx
@@ -1,28 +1,28 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
-import massAttendance from "@/pages/massAttendance";
+import MassAttendance from "@/pages/massAttendance";
 
 describe("Mass Attendance page", () => {
   it("includes a header", () => {
-    render(<massAttendance />);
+    render(<MassAttendance />);
     expect(screen.getByTestId("headerSection")).toBeInTheDocument;
   });
 
   it("includes a footer", () => {
-    render(<massAttendance />);
+    render(<MassAttendance />);
     expect(screen.getByTestId("footing")).toBeInTheDocument;
   });
 
   it("has the correct title", () => {
-    render(<massAttendance />);
+    render(<MassAttendance />);
     expect(screen.getByTestId("massAttendancePageTitle")).toHaveTextContent(
       "Mass Attendance"
     );
   });
 
   it('includes a "Mass Attendance" component', async () => {
-    render(<massAttendance />);
+    render(<MassAttendance />);
 
     const massAttendanceSection = await screen.findByTestId(
       "massAttendanceChart"
